e2d3672a9b83ff35a21b6460152e2388d101bc63
AlexHoorn <12898097+AlexHoorn@users.noreply.github.com>
2022-03-17 12:09:32 +0100

    Geonames ontology

--- geonames/create_geo_graph.py
+++ geonames/create_geo_graph.py
@@ -1,20 +1,36 @@
 # %% Packages
 import logging
+from os import path
 
 import pandas as pd
-from rdflib import RDFS, ConjunctiveGraph, Literal, Namespace
+from rdflib import OWL, RDF, RDFS, ConjunctiveGraph, Literal, Namespace
 from tqdm import tqdm
 
 logging.basicConfig(
     format="%(asctime)s %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S"
 )
 
-# %% Function to add a relation from a pandas series
+base_dir = path.dirname(__file__)
+countries_file = path.join(base_dir, "allCountries.txt")
+relations_file = path.join(base_dir, "hierarchy.txt")
+
+# %% Functions to information from pandas series
 def add_relation(row: pd.Series, g: ConjunctiveGraph, ns: Namespace) -> None:
     s = getattr(ns, str(row.childId))
     o = getattr(ns, str(row.parentId))
 
     g.add((s, ns.partOf, o))
+
+
+def add_country(row: pd.Series, g: ConjunctiveGraph, ns: Namespace) -> None:
+    s = getattr(ns, str(row.name))
+
+    g.add((s, RDFS.label, Literal(row["name"])))
+    g.add((s, ns.countryCode, Literal(row["country code"])))
+    g.add((s, ns.latitude, Literal(row["latitude"])))
+    g.add((s, ns.longitude, Literal(row["longitude"])))
+    g.add((s, ns.featureClass, Literal(row["feature class"])))
+    g.add((s, ns.featureCode, Literal(row["feature code"])))
 
 
 # %% Load dataframe of all geonames entities (~12mil rows)
@@ -41,17 +57,33 @@ all_countries_cols = [
 ]
 
 logging.info("Loading allCountries dataframe")
-countries = pd.read_csv(
-    "allCountries.txt", sep="\t", names=all_countries_cols, index_col=0
-)
-countries.head()
+# Download allCountries.txt from https://download.geonames.org/export/dump/
+countries = pd.read_csv(countries_file, sep="\t", names=all_countries_cols, index_col=0)
+
+# %% Keep only features we are interested in: http://www.geonames.org/export/codes.html
+keep_features = ["A", "H", "L", "P", "T", "U", "V"]
+
+n_before = len(countries)
+countries = countries[countries["feature class"].isin(keep_features)]
+
+logging.info(f"Filtered {n_before - len(countries)} unnecessary rows")
 
 # %% Load dataframe of all geonames hierarchy relations
 logging.info("Loading relations dataframe")
-relations = pd.read_csv(
-    "hierarchy.txt", sep="\t", names=["parentId", "childId", "type"]
-)
-relations.head()
+# Download hierarchy.txt from https://download.geonames.org/export/dump/
+relations = pd.read_csv(relations_file, sep="\t", names=["parentId", "childId", "type"])
+
+# %% Only keep relations also in countries dataframe
+relations = relations[
+    relations["parentId"].isin(countries.index)
+    & relations["childId"].isin(countries.index)
+]
+
+# %% Only keep countries for which we have relations
+countries = countries[
+    countries.index.isin(relations["parentId"])
+    | countries.index.isin(relations["childId"])
+]
 
 # %% Create new empty graph
 logging.info("Setting up new graph")
@@ -60,32 +92,22 @@ gn = Namespace("http://www.geonames.org/ontology#")
 
 g.namespace_manager.bind("gn", gn)
 
+# Make partOf transitive
+g.add((gn.partOf, RDF.type, OWL.TransitiveProperty))
+
 # %% Add entity information to graph
-for geo_id in tqdm(
-    # NOTE: Currently only entities with relations are added
-    set(relations.parentId) | set(relations.childId),
-    desc="Adding general subject information",
-):
-    try:
-        row = countries.loc[geo_id]
-        s = getattr(gn, str(geo_id))
-
-        g.add((s, RDFS.label, Literal(row["name"])))
-        g.add((s, gn.countryCode, Literal(row["country code"])))
-        g.add((s, gn.latitude, Literal(row["latitude"])))
-        g.add((s, gn.longitude, Literal(row["longitude"])))
-        g.add((s, gn.featureClass, Literal(row["feature class"])))
-        g.add((s, gn.featureCode, Literal(row["feature code"])))
-
-    except KeyError:
-        pass
+tqdm.pandas(desc="Adding general subject information")
+_ = countries.progress_apply(add_country, axis=1, g=g, ns=gn)
 
 # %% Add hierarchical relations
 tqdm.pandas(desc="Adding partOf relations")
 _ = relations.progress_apply(add_relation, axis=1, g=g, ns=gn)
 
 # %% Store graph to xml/turtle
-logging.info("Saving graph to xml")
-g.serialize("geonames.xml", format="xml")
 logging.info("Saving graph to turtle")
-g.serialize("geonames.ttl", format="turtle")
+g.serialize(path.join(base_dir, "geonames.ttl"), format="turtle")
+
+# %% Store processed dataframes for later use
+logging.info("Saving processed data to csv")
+countries.to_csv(path.join(base_dir, "allCountries_processed.csv"))
+relations.to_csv(path.join(base_dir, "hierarchy_processed.csv"), index=False)
