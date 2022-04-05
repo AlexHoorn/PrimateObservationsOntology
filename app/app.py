e50181fcf1773c09e841bc4f974b8025c9fdf43a
AlexHoorn <12898097+AlexHoorn@users.noreply.github.com>
2022-04-05 15:42:58 +0200

    Comment

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from pages import pages
 
+# To run this make sure you have streamlit installed and run `streamlit run app.py`
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
 
