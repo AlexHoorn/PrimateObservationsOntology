fe854dfceb6891b3de73d8354a8f303ac2150edd
Sunny Soni <sunnysoni97@gmail.com>
2022-04-08 19:02:54 +0200

    added sparql endpoint gui

--- app/pages/__init__.py
+++ app/pages/__init__.py
@@ -1,4 +1,5 @@
 from .map_all import page_map_all
 from .query_test import page_query_test
+from .sparql_endpoint import sparql_endpoint
 
-pages = {"All observations": page_map_all, "Query test": page_query_test}
+pages = {"All observations": page_map_all, "Query test": page_query_test,"Sparql Endpoint": sparql_endpoint}
