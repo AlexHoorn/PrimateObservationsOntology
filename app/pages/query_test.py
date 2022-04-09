d71bb3125c7c2647e9b13f7bee45457a38ca69e6
AlexHoorn <12898097+AlexHoorn@users.noreply.github.com>
2022-04-09 15:00:55 +0200

    Add a config

--- app/pages/query_test.py
+++ app/pages/query_test.py
@@ -1,19 +1,8 @@
-import pandas as pd
 import streamlit as st
-from rdflib import ConjunctiveGraph
+
+from .utils import sparql_query
 
 
 def page_query_test():
-    g = ConjunctiveGraph()
-    res = g.query(
-        """
-        SELECT * WHERE {
-        SERVICE <https://api.triplydb.com/datasets/weiyuxinghen/primate-database/services/primate-database/sparql> {
-            ?s ?p ?o
-            }
-        }
-        """,
-    )
-
-    df = pd.DataFrame(res)
-    st.dataframe(df)
+    df = sparql_query("SELECT * WHERE {?s ?p ?o}")
+    st.write(df)
---
+++ app/pages/utils.py
@@ -0,0 +1,17 @@
+import os
+from os import path
+
+import yaml
+from pandas import DataFrame
+from SPARQLWrapper import get_sparql_dataframe
+
+config_file = path.join(path.dirname(__file__), os.pardir, "config.yaml")
+
+with open(config_file, "r", encoding="utf-8") as f:
+    config = yaml.safe_load(f)
+
+sparql_endpoint: str = config["sparql_endpoint"]
+
+
+def sparql_query(query: str) -> DataFrame:
+    return get_sparql_dataframe(sparql_endpoint, query)
