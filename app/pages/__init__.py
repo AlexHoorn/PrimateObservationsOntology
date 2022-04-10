6dbb48a1e5fa6c88145d31b424581d18b1bedcdb
AlexHoorn <12898097+AlexHoorn@users.noreply.github.com>
2022-04-10 22:47:45 +0200

    Remove old test

--- app/pages/__init__.py
+++ app/pages/__init__.py
@@ -2,7 +2,6 @@ from typing import Callable, Dict
 
 from .distribution_obs import distribution_obs
 from .homepage import page_home
-from .query_test import page_query_test
 from .sparql_endpoint import sparql_endpoint
 from .observations_count import page_observations_count
 from .observations_trend import page_observations_trend
@@ -12,6 +11,5 @@ pages: Dict[str, Callable] = {
     "📊 Observation counts": page_observations_count,
     "Distribution of Observations": distribution_obs,
     "📈 Trend of observations": page_observations_trend,
-    "🧪 Query test": page_query_test,
-    "🔬 Sparql Endpoint": sparql_endpoint,
+    "🧪 Sparql Endpoint": sparql_endpoint,
 }
