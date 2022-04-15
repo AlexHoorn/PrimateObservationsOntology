e7522f835d71ec28dd388eed39350fe48620e80d
AlexHoorn <12898097+AlexHoorn@users.noreply.github.com>
2022-04-15 13:31:41 +0200

    Remove old or irrelevant stuff

--- fetcher_api/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-import os
-
-os.environ["GEONAMES_USERNAME"] = "alexhn"
-
-# To surpress all the rdflib serialization warnings
-logger = logging.getLogger("rdflib")
-logger.setLevel(logging.ERROR)
